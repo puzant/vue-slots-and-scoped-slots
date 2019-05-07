0036b95a8ec8dc700eb50233816da0ead6132c31
Puzant <PBAKJEJIAN@hotmail.com>
2019-05-07 22:01:31 +0300

    pushing all the code

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="VueJs frameWork" />
+    <hello>
+      <img slot="vuejs-image" width="25%" src="./assets/logo.png">
+
+      <h2 slot="line1">hello there</h2>
+      <hr slot="seperator">
+      <h3 slot="line2">this is VueJs slots test</h3>
+
+      <a href="" slot="link1">link1</a>
+      <a href="" slot="link2">link2</a>
+      <a href="" slot="link3">link3</a>
+
+
+      <p slot="paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+      <button class="btn" slot="btn" @click="greet()">Click me</button>
+      <!-- accessing the scoped slot from the child -->
+      <p slot-scope="defaultSlotText">{{defaultSlotText.text}}</p>
+
+      <h2 slot="exmaple-title">another example with scoped slots</h2>
+
+      <div slot="notificationData" slot-scope="props">
+        <div class="notification-container" v-for="(item) in props.notifications">
+          <p class="notification" :style=" { background: item.hexColor } ">{{item.name}}</p>
+        </div>
+      </div> 
+
+    </hello>
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello";
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    Hello,
+    HelloWorld
+  },
+  methods: {
+    greet() {
+      alert("hello there,I am greeting you from the parent");
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+.btn {
+  border: 1px solid #fff;
+  color: #fff;
+  background-color: rgb(67, 145, 99);
+  border-radius: 5px;
+  padding: 5px;
+}
+
+.btn:focus {
+  border: none;
+  outline: none;
+}
+
+hr {
+  width: 50%;
+}
+
+.notification-container {
+  display: flex;
+  justify-content: center;
+}
+
+.notification {
+  margin: 7px;
+  font-weight: bold;
+  color: #111;
+  padding: 4px;
+  width: 30%;
+  border-radius: 5px;
+}
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <!-- add the image here as slot -->
+    <slot name="vuejs-image"></slot>
+    <slot name="line1"></slot>
+    <slot name="seperator"></slot>
+    <slot name="line2"></slot>
+
+    <slot name="link1"></slot>
+    <slot name="link2"></slot>
+    <slot name="link3"></slot>
+
+    <slot name="paragraph"></slot>
+    <slot name="btn"></slot>
+    <!-- scoped slot -->
+    <slot :text="defaultSlotText"></slot>
+
+    <ul v-for="s in shapes">
+      <li :style=" { background: s.hex } " @click="tellMeShapeWithColor(s)">{{s.name}}, {{s.sides}}, {{s.hex}}</li>
+    </ul>
+
+    <slot name="exmaple-title"></slot>
+    
+    <slot name="notificationData" :notifications="notifications"></slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      defaultSlotText: "I'll get rendered inside the default slot!.",
+      infoMessage: "hello there this is info message",
+      shapes: [
+        { name: "Square", sides: 4, hex: "#F4D03F", colorName: 'Saffron' },
+        { name: "Hexagon", sides: 6, hex: "#229954", colorName: 'Eucalyptus'},
+        { name: "Triangle", sides: 3, hex: "#9B59B6", colorName: 'Wisteria' }
+      ],
+      notifications: [
+        {name: 'warning', hexColor: '#FA3E3E'},
+        {name: 'info', hexColor: '#005EB2'},
+        {name: 'error', hexColor: '#FFCA00'},
+      ]
+    };
+  },
+
+  methods: {
+    tellMeShapeWithColor(s) {
+      alert('shape is ' + s.name + '\n' + 'color is ' + this.findTheColorName(s))
+    },
+
+    findTheColorName(s) {
+     return s.colorName
+    }
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 20px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+  margin: 20px;
+}
+
+li {
+  color: #111;
+  padding: 4px;
+  border-radius: 5px;
+  width: 30%;
+  font-weight: bold;
+
+}
+</style>
